c9afa4d67e0a1db2030e637e848fe755f3cbfdb8
xin <522079893@qq.com>
2017-12-06 22:58:36 +0800

    init commit

---
+++ backend/controllers/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/5 0005
+ * Time: 21:40
+ */
+
+namespace backend\controllers;
+
+
+use yii\web\Controller;
+
+class UsersController extends Controller
+{
+    public function actionIndex(){
+
+    }
+}
